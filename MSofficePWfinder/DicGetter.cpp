d5e3a2176d2ef49d8b5e25402ecde833ee167878
patternplane <bss011202@naver.com>
2024-01-09 17:55:05 +0900

    [FEAT] 사용자 파일로부터 자료 가져오는 모듈 추가

---
+++ MSofficePWfinder/DicGetter.cpp
@@ -0,0 +1,44 @@
+#include "DicGetter.h"
+
+DictionData* getDiction() {
+	FILE* fp;
+	char filePath[500];
+	strcpy_s(filePath, USER_INPUT_DIR_SHORT);
+	strcat_s(filePath, USER_INPUT_FILE);
+	int errorCode = fopen_s(&fp, filePath,"r");
+
+	if (errorCode != 0) {
+		printf("\n\n");
+		printf("  ▲ 예상 비밀번호를 기입한 파일을 준비해주세요!\n  (위치 : 프로그램이 있는 폴더의 input 폴더 내부에 input.txt 파일을 생성하기)\n");
+		return NULL;
+	}
+
+	char fileContent[2500];
+	int readLen = fread(fileContent,sizeof(char),5000,fp);
+	fileContent[readLen] = '\0';
+
+	char* passwords[100];
+	int cnt = 0;
+	char* temp;
+	char* token = strtok_s(fileContent, "\n", &temp);
+	while (token != NULL) {
+		passwords[cnt++] = token;
+		token = strtok_s(NULL, "\n", &temp);
+	}
+
+	DictionData* result = (DictionData*)malloc(sizeof(DictionData));
+	result->amount = cnt;
+	result->diction = (PWCHAR*)malloc(cnt * sizeof(PWCHAR));
+	for (int i = 0; i < cnt; i++) {
+		int len = 0;
+		for (; passwords[i][len] != '\0'; len++);
+		result->diction[i] = (PWCHAR)malloc((len + 1) * sizeof(WCHAR));
+		for (int j = 0; ; j++) {
+			result->diction[i][j] = (WCHAR)(passwords[i][j]);
+			if (passwords[i][j] == '\0')
+				break;
+		}
+	}
+
+	return result;
+}
